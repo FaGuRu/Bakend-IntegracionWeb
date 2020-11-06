5729c84b172c55f55e1065725aecc5f08dfe0c1a
FaGuRu <193247@ids.upchiapas.edu.mx>
2020-11-05 23:35:48 -0600

    Url.py change

--- Profile/urls.py
+++ Profile/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # re_path(r'^profileModel_url', views.ProfileModelView.as_view()),
     re_path(r'^userModel_url',views.UsersViewset.as_view()),
     re_path(r'^userModel_post/',views.UsersViewset.as_view()),
-    path(r'userModel_delete/<int:pk>/',views.UsersViewset.as_view()),
-    path(r'userModel_update/<int:pk>/',views.UsersViewset.as_view()),
+    path('userModel_delete/<int:pk>/',views.UsersViewset.as_view()),
+    path('userModel_update/<int:pk>/',views.UsersViewset.as_view()),
 ]
